ce719a57865bd357649a795411951aa545214c8c
RobertPorteles <robertporteless@gmail.com>
2025-09-30 15:56:26 -0300

    fix: ajuste em ClienteEntity (descrição curta)

--- src/main/java/br/com/nildoSantos/domain/entities/ClienteEntity.java
+++ src/main/java/br/com/nildoSantos/domain/entities/ClienteEntity.java
@@ -1,13 +1,17 @@
 package br.com.nildoSantos.domain.entities;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
-import org.springframework.cglib.core.Local;
 
 @Entity
 @Data
